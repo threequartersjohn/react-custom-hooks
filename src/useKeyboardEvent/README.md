00d791677cf3d00119521c71b2114ace3b9dedc3
João Silva <joaosilva@Joao-Silvas-MBP.lan>
2021-12-18 20:28:25 +0000

    feat: restructure package, add useKeyboardEvent options and documentation

---
+++ src/useKeyboardEvent/README.md
@@ -0,0 +1,24 @@
+## useKeyboardEvent
+
+React hook to register keyboard events. 
+
+### Usage
+
+```js
+useKeyboardEvent(
+    key : String | String[],
+    callback: Function,
+    options?: {
+        event: String           // One of `keyup`, `keydown` or `keypress`.
+    })
+```
+
+```js
+import { useState } from 'react';
+
+const [count, setCount] = useState(0);
+
+const handleOnIncreaseCount = () => setCount(count + 1);
+
+useKeyboardEvent('ArrowUp', handleOnIncreaseCount);
+```
---
+++ src/useKeyboardEvent/index.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const keyboardEvents = [
+    'keyup',
+    'keydown',
+    'keypress',
+]
+
+export const useKeyboardEvent = (keys, callback, { event = 'keydown' }) => {
+    if (!keyboardEvents.includes(event)) {
+        throw Error('Non keyboard related event requested. Please use one of `keyup`, `keydown` or `keypress`.');
+    }
+
+    useEffect(() => {
+        const keysArr = Array.isArray(keys) ? keys : [keys];
+
+        const eventCallback = (event) => keysArr.includes(event.key) && callback()
+                
+        callback && window.addEventListener(event, eventCallback);
+
+        return () => callback && window.removeEventListener(event, eventCallback)
+    })
+}
